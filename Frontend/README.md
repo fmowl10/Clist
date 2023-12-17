723c08c5f8b097fdf8545502a7bd34c5f0892a20
nanana3679 <nanana3679@gmail.com>
2023-12-17 14:22:15 +0900

    add environment variable

--- Frontend/README.md
+++ Frontend/README.md
@@ -1,2 +1,9 @@
 # Clist
 공돌이 옷입히기 - 사용자의 옷을 관리해주고 기온이나 옷 색의 조합을 기반으로 오늘 입고 나갈 옷을 추천해주는 옷장 서비스 - 명칭 배경
+
+# ENVs
+REACT_APP_AUTHTOKEN
+REACT_APP_AUTH_PORT
+REACT_APP_LOGIC_PORT
+REACT_APP_TMP_ID
+REACT_APP_HASURA_ENDPOINT
--- Frontend/src/util/auth.js
+++ Frontend/src/util/auth.js
@@ -1,23 +1,23 @@
 export function getId() {
-  return "c025f706-820e-11ee-b962-0242ac120002";
+  return process.env.REACT_APP_TMP_ID;
 }
 
 export function getAuthToken() {
-  return "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJsb2MiOiLsnbjsspwiLCJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWRlZmF1bHQtcm9sZSI6InVzZXIiLCJ4LWhhc3VyYS1hbGxvd2VkLXJvbGVzIjpbInVzZXIiLCJhZG1pbiJdLCJ4LWhhc3VyYS11c2VyLWlkIjoiYzAyNWY3MDYtODIwZS0xMWVlLWI5NjItMDI0MmFjMTIwMDAyIn19.kqL8_oDsTyU7g-guRWRRUZ_07ZyYT3Kjt_MGzkENQmcxeR5SUOTa-W7Y2FEJB0kD0oP0DAD1VSr7-8l3MV385w";
+  return process.env.REACT_APP_AUTHTOKEN;
 }
 
 export function getRecommendToken() {
-  return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJsb2NhdGlvbiI6IlNFT1VMIiwiaHR0cHM6Ly9oYXN1cmEuaW8vand0L2NsYWltcyI6eyJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtYWxsb3dlZC1yb2xlcyI6WyJ1c2VyIiwiYWRtaW4iXSwieC1oYXN1cmEtdXNlci1pZCI6ImMwMjVmNzA2LTgyMGUtMTFlZS1iOTYyLTAyNDJhYzEyMDAwMiJ9fQ.Po0Y9T59tCyKcFnb7_08IycwNXQPin6IrBWBk9oQ6Ds";
+  return process.env.REACT_APP_LOGICTOKEN;
 }
 
 export function getGraphqlEndpoint() {
-  return "https://fmowl.narumir.io/v1/graphql";
+  return process.env.REACT_APP_HASURA_ENDPOINT;
 }
 
 export function getAuthServerEndpoint() {
-  return "http://127.0.0.1:80/";
+  return `http://127.0.0.1:${process.env.REACT_APP_AUTH_PORT}/auth/`;
 }
 
 export function getLogicServerEndpoint() {
-  return "http://127.0.0.1:8080/";
+  return `http://127.0.0.1:${process.env.REACT_APP_LOGIC_PORT}/logic/`;
 }
